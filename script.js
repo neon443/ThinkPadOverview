3a6b2113f44312956535dc9054fab4feef7fc8f6
neon443 <69979447+neon443@users.noreply.github.com>
2024-12-08 16:18:05 +0000

    loading animation lets goooooo ui color changes, jpeg->png with crops

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
 	const menuToggle = document.getElementById('menu-toggle');
 	const mobileMenu = document.getElementById('mobile-menu');
+	const sections = document.querySelectorAll('section');
 
 	menuToggle.addEventListener('click', function() {
 		console.log('menu toggle clicked');
 		mobileMenu.classList.toggle('open');
+	});
+
+	// fly in anim
+	sections.forEach((section, index) => {
+		section.style.animationDelay = `${index * 250}ms`;
+		section.classList.add('fly-in');
 	});
 });
